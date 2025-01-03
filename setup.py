094873e9dfaa46aeed140e34718ee2fec4aa0727
Zarine Hayat Choudhury <hayatzarine@gmail.com>
2025-01-03 10:47:28 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alarm-clock",  # Package name
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here
+    description="A simple alarm clock implemented in Python",
+    author="Zarine",
+    author_email="your_email@example.com",
+    url="https://github.com/yourusername/alarm-clock",
+)
